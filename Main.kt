b61d4e8c93e61c35632839bd247892db92426b10
Rebe HC <161864810+RebecaH4@users.noreply.github.com>
2025-05-06 13:02:04 -0600

    Add files via upload

---
+++ Main.kt
@@ -0,0 +1,5 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main(parametro: Array<String>) {
+    print("Hello word")
+}
