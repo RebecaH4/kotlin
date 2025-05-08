57e02f6572f99400ece6a6b0503e7705972b0b8c
Rebe HC <161864810+RebecaH4@users.noreply.github.com>
2025-05-08 08:58:41 -0600

    Add files via upload

---
+++ Project2.kt
@@ -0,0 +1,10 @@
+fun main (parametro: Array<String>){
+    var value1 = 100
+    var value2 = 400
+
+    var result = value1 + value2
+    println("El resultado de la suma de $value1 + $value2 es $result")
+
+    result = value1 * value2
+    println("El resultado de la multiplicacion de $value1 * $value2 es $result")
+}
