0a424560230eee69d54bdc6fff51b40c56c11855
Rebe HC <161864810+RebecaH4@users.noreply.github.com>
2025-05-09 10:33:46 -0600

    Add files via upload

---
+++ Project3.kt
@@ -0,0 +1,7 @@
+fun main (parametro: Array<String>){
+    var lado = 50
+    var perimetro = lado * 4
+    var superficie = lado * lado
+
+    println("El perimetro del cuadrado es $perimetro y su area es de $superficie")
+}
